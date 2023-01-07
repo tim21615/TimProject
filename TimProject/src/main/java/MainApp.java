d3dbe89932910c7c991769c8bb67848cc2d533ff
TimLee <tim21615@gmail.com>
2023-01-07 14:11:52 +0800

    新增 print HelloWorld 功能

--- TimProject/src/main/java/MainApp.java
+++ TimProject/src/main/java/MainApp.java
@@ -3,7 +3,13 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		²Õªø
+		System.out.print("1st Version");
+		printHelloWorld();
 
 	}
 
+	static void printHelloWorld() {
+		System.out.println("Hello World");
+	}
 }
